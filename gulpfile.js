6e8bdb7e5022eb896d249a672eea00a58e91f0b5
Logan Harber <chalharb@gmail.com>
2016-08-16 20:28:42 -0500

    created development directory and assets directory. Created package.json and gulpfile for node_modules

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Logan Harber on August 16, 2016
+ */
+
+var elixir = require('laravel-elixir');
+
+elixir.config.assetsPath = '_dev';
+elixir.config.publicPath = 'assets';
+
+elixir(function(mix) {
+    mix.sass('styles.scss')
+});
+
+elixir(function(mix) {
+    mix.browserify('main.js');
+});
